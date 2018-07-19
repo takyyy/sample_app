8be67e4503fc898f1154607f803172cd33a7bc38
takyyy <tkytkymtd@gmail.com>
2018-07-20 07:34:06 +0900

    Add pagenation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
   def show
     @user = User.find(params[:id])
@@ -18,6 +18,10 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
   end
 
   def edit
