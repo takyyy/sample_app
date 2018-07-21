e082266b16f480392eb80cefbb172531413ebd3b
takyyy <tkytkymtd@gmail.com>
2018-07-21 23:50:55 +0900

    Finish user edit, update, index, and destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+  
   def show
     @user = User.find(params[:id])
   end
@@ -34,6 +36,12 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
   private
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,41 +7,26 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @other_user = users(:archer)
   end
 
-  test "should get new" do
-    get signup_path
-    assert_response :success
-  end
+  test "index as admin including pagination and delete links" do
+   log_in_as(@admin)
+   get users_path
+   assert_template 'users/index'
+   assert_select 'div.pagination'
+   first_page_of_users = User.paginate(page: 1)
+   first_page_of_users.each do |user|
+     assert_select 'a[href=?]', user_path(user), text: user.name
+     unless user == @admin
+       assert_select 'a[href=?]', user_path(user), text: 'delete'
+     end
+   end
+   assert_difference 'User.count', -1 do
+     delete user_path(@non_admin)
+   end
+ end
 
-  test "should redirect edit when not logged in" do
-    get edit_user_path(@user)
-    assert_not flash.empty?
-    assert_redirected_to login_url
-  end
-
-  test "should redirect update when not logged in" do
-    patch user_path(@user), params: { user: { name: @user.name,
-                                             email: @user.email } }
-    assert_not flash.empty?
-    assert_redirected_to login_url
-  end
-
-  test "should redirect index when not logged in" do
-    get users_path
-    assert_redirected_to login_url
-  end
-
-  test "should redirect edit when logged in as wrong user" do
-    log_in_as(@other_user)
-    get edit_user_path(@user)
-    assert flash.empty?
-    assert_redirected_to root_url
-  end
-
-  test "should redirect update when logged in as wrong user" do
-    log_in_as(@other_user)
-    patch user_path(@user), params: { user: { name: @user.name,
-                                              email: @user.email } }
-    assert flash.empty?
-    assert_redirected_to root_url
-  end
+ test "index as non-admin" do
+   log_in_as(@non_admin)
+   get users_path
+   assert_select 'a', text: 'delete', count: 0
+ end
 end
