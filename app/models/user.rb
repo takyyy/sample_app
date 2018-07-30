5ef5c1424f63e9a56da9033c7c9ec5f0fbb12f2a
takyyy <tkytkymtd@gmail.com>
2018-07-31 07:37:39 +0900

    Add password resets flow

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save   {email.downcase!}
   before_create :create_activation_digest
   validates :name, presence: true, length: {maximum: 50}
@@ -50,12 +50,24 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
 
+  # パスワード再設定の属性を設定する
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # パスワード再設定のメールを送信する
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
   private
 
   # メールアドレスをすべて小文字にする
-  # def downcase_email
-  #   self.email = email.downcase
-  # end
+   def downcase_email
+     self.email = email.downcase
+   end
 
   # 有効化トークンとダイジェストを作成および代入する
     def create_activation_digest
